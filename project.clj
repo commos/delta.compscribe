0a76134646f1bcb26b122a9399b7be09d7300203
lgrapenthin <grapenthin.leon@gmail.com>
2015-08-14 14:38:41 +0200

    upgrade ultimate language impls

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/commos/delta.compscribe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  
                  [org.commos/service "0.1.0"]
@@ -11,7 +11,7 @@
                  [org.commos/delta.cache "0.1.0"]
                  [org.commos/shared "0.1.0"]]
   :source-paths ["src/cljc"]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-3269"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.7.48"]]
                    :plugins [[lein-cljsbuild "1.0.6"]]
                    :test-paths ["test/cljc"]
                    :cljsbuild
