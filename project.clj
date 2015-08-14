2bca35cd841df7a6d6dc65460061c0c3f3a07ec8
lgrapenthin <grapenthin.leon@gmail.com>
2015-08-14 14:58:22 +0200

    upgrade commos deps

--- project.clj
+++ project.clj
@@ -6,10 +6,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  
-                 [org.commos/service "0.1.0"]
-                 [org.commos/delta "0.3.0"]
-                 [org.commos/delta.cache "0.1.0"]
-                 [org.commos/shared "0.1.0"]]
+                 [org.commos/service "0.2.0"]
+                 [org.commos/delta "0.3.1"]
+                 [org.commos/delta.cache "0.1.1"]]
   :source-paths ["src/cljc"]
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.7.48"]]
                    :plugins [[lein-cljsbuild "1.0.6"]]
